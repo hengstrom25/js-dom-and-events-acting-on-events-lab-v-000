1caaf904d3476190de6c2cf959472bf78bfa171c
Heidi Engstrom <hengstrom25@gmail.com>
2018-12-30 19:58:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function addNewElementAsLi () {
   let element = document.createElement('li');
   element.innerHTML = retrieveEmployeeInformation();
   let list = document.querySelector('.employee-list');
-  
+  list.append(element);
 }
 

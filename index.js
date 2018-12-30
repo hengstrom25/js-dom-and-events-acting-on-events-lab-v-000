8c0686e7629dca428114f3b1ffd7774187d52306
Heidi Engstrom <hengstrom25@gmail.com>
2018-12-30 19:59:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation() {
-  return document.querySelector('input').value;
+  return input.value;
 }
 
 function addNewElementAsLi() {

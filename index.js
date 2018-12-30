7f46723c5f55dbff58e8b2232f48aca9fd5666d0
Heidi Engstrom <hengstrom25@gmail.com>
2018-12-30 20:05:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function addNewElementAsLi() {
 }
 
 function addNewLiOnClick() {
-  let submit = document.querySelector('input[type = submit]')
+  let submit = document.querySelector('input[type = 'submit']')
 }

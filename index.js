cd43db2362d9aa8fdfc82d2d0649f0704c6b119b
Heidi Engstrom <hengstrom25@gmail.com>
2018-12-30 20:09:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,17 @@ function addNewElementAsLi() {
 }
 
 function addNewLiOnClick() {
-  let submit = document.querySelector('input[type = 'submit']');
+  let submit = document.querySelector("input[type = 'submit'");
   submit.addEventListener('click', function(e) {
     addNewElementAsLi();
     input.value = "";
   });
 }
+
+function addNewLiOnClick() {
+   let submit = document.querySelector("input[type= 'submit'");
+   submit.addEventListener('click', function(e) {
+     addNewElementAsLi();
+     input.value = '';
+   });
+ }
